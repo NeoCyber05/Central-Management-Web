bdf4a9c0068114b96078a232e9d8102ad942e026
NeoCyber05 <bach14062005@gmail.com>
2025-03-30 12:02:48 +0700

    create init_roles.py

--- management/models.py
+++ management/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('staff', 'Nhân viên'),
+        ('teacher', 'Giáo viên'),
+        ('student', 'Học viên'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
 
 # 1. Bảng học viên
 class HocVien(models.Model):
